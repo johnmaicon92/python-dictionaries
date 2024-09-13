eb2e11f18db7c20fcc835b55f941fd5016700b65
johnmaicon92 <johnmaicon_sempre@hotmail.com>
2024-09-12 21:04:55 -0400

    Final

--- python-dictionaries.py
+++ python-dictionaries.py
@@ -31,7 +31,8 @@ print(restaurant_menu)
 
 
 """
-2. Python Programming Challenges for Customer Service Data Handling
+Question 2. Python Programming Challenges for Customer Service Data Handling
+
 Task 1: Customer Service Ticket Tracker Demonstrate your ability to use nested collections and loops by creating a system to track customer service tickets.
 
 Problem Statement: Develop a program that:
@@ -59,17 +60,62 @@ service_tickets = {
 }
 
 
-def new_service_ticket():
-    number = int(input("Please enter the number of the new ticket: "))
-    print(f"Ticket{number}:")
-new_service_ticket()
+def new_service_ticket(tickets, ticket_num, customer, issue):
+    new_ticket = "Ticket" + ticket_num
+    if new_ticket in tickets:
+        print(f"{new_ticket} already exists in our system.")
+    else:
+        tickets[new_ticket] = {"Customer": customer, "Issue": str.capitalize(issue), "Status": "open"}
+        print(f"'{new_ticket}' has been added for '{customer}' with the issue '{str.capitalize(issue)}'")
 
 
-def update_status():
-    pass
+def update_status(tickets, ticket_num, status):
+    new_ticket = "Ticket"+ticket_num
+    if new_ticket in tickets:
+        tickets[new_ticket]["Status"] = status
+        print(f"Ticket{new_ticket} status updated to {status}.")
+    else:
+        print(f"{new_ticket} doesn't exist.")
 
 
-def display_tickets():
-    for ticket in service_tickets:
-        print(f"{ticket}: Customer - {service_tickets[ticket]['Customer']}, Issue - {service_tickets[ticket]['Issue']}, Status - {service_tickets[ticket]['Status']}")
-display_tickets()
+def display_tickets(tickets, choice = None):
+    if choice:
+        filtered_tickets = {id: ticket for id, ticket in tickets.item() if ticket['Status'] == choice}
+        if filtered_tickets:
+            for id, ticket in filtered_tickets.items():
+                print(f"{id}: Customer: {ticket['Customer']}, Issue: {ticket['Issue']}")
+        else:
+            print(f"No tickets found with status '{choice}'.")
+    else:
+        print("All tickets:")
+        for id, ticket in tickets.items():
+            print(f"{id}: Customer: {ticket['Customer']}, Issue: {ticket['Issue']}, Status: {ticket['Status']}")
+    
+
+
+while True:
+    print("-----------------------------------------")
+    print("Ticket Menu:")
+    print("-----------------------------------------")
+    print("1. Open a new service ticket.")
+    print("2. Update status.")
+    print("3. Display tickets or filter by status.")
+    print("4. Exit.")
+    user_input = input("Please select one of our menu options: ")
+    if user_input == "1":
+        user_input1 = input("Please Enter ticket number: ")
+        user_input2 = input("Customers name: ").title()
+        user_input3 = input("Customers Issue: ")
+        new_service_ticket(service_tickets, user_input1, user_input2, user_input3)
+    elif user_input == "2":
+        user_input1 = input("Please enter ticket number: ")
+        user_input2 = input("What do you want to change status to: [OPEN/CLOSED] ").lower()
+        update_status(service_tickets, user_input1, user_input2)
+    elif user_input == "3":
+        user_input1 = input("Open, Closed or Press Enter for all: ").lower()
+        display_tickets(service_tickets, user_input1)
+    elif user_input == "4":
+        print("Thank you for using Service Ticket Handling Program")
+        break
+    else:
+        print("Invalid input try again")
